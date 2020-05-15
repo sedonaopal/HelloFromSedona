82ff285252d4d2e56fb60c6e44ff3db547613ea7
Wesley <wesleyburgener@gmail.com>
2020-05-15 17:15:07 -0600

    Adding message to Sedona's file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Sedona.");
+
+        System.out.println("Hello, my name is Sedona.");
+        System.out.println("Hello Tedona Sooley, my name is Wes.");
     }
 }
